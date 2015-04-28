d1615ffbbc9ea59b50c7b3fb062082510acd0534
David Jonathan Garber & Chris Sefkin <student@epicodus.com>
2015-04-28 09:43:35 -0700

    saves tasks to the array. passes test.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -1,6 +1,7 @@
 require ('rspec')
 require ('task')
 
+
 describe(Task) do
   describe("#desciption") do
     it("lets you give a description") do
@@ -11,6 +12,13 @@ describe(Task) do
   describe(".all") do
     it("is empty at first") do
       expect(Task.all()).to(eq([]))
+    end
+  end
+  describe("#save") do
+    it("adds a task to the array saved tasks") do
+      test_task = Task.new("wash the lion")
+      test_task.save()
+      expect(Task.all()).to(eq([test_task]))
     end
   end
 end
